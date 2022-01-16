53e9ea10cfee93b56c0580c354c51e4720519243
Aleksandr Minaev <veselij85@gmail.com>
2022-01-16 21:21:21 +0300

    added STATIC_ROOT

--- ogo/ogo/settings.py
+++ ogo/ogo/settings.py
@@ -117,6 +117,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
